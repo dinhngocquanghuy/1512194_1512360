819ad0f0feb5921914f725d2cadaf25a387ceb26
dinhngocquanghuy <dinhngocquanghuy@gmail.com>
2019-05-22 22:02:14 +0700

    huydnq

---
+++ public/javascripts/meta_model.js
@@ -0,0 +1,38 @@
+function Windows()
+{
+  this.width = 0;
+  this.height = 0;
+  this.link_background = '';
+}
+
+function Section()
+{
+  this.id = '';
+  this.class = '';
+  this.order = 0;
+  this.x = 0;//xác định dựa theo vị trí của windows
+  this.y = 0;//relative
+  this.width = 0;
+  this.height = 0;
+  this.content = {};
+}
+
+//button, checkbox, input sẽ tạo ra các lớp Button, Checkbox, Input tương ứng và kế thừa lớp này
+function Control()
+{
+  this.id = '';
+  this.class = '';
+  this.belong_to = {}; //xác định bằng id hoặc class của section, cú pháp gán belong_to['id'] = 'abc' hoặc belong_to['class'] = 'abc';
+  this.x = 0;//xác định dựa theo vị trí của section
+  this.y = 0;//relative
+}
+
+function Gridview()
+{
+
+}
+
+function List()
+{
+
+}
